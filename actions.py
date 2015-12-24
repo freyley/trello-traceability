afba81cdae4d6947965fd042a0286faa02e1b0d1
Jeff Schwaber <jschwaber@mobilepaks.com>
2015-12-23 22:01:05 -0800

    towards usability

--- actions.py
+++ actions.py
@@ -1,12 +1,5 @@
 import sys
 
-def quit():
-    sys.exit(0)
-
-def help():
-    for action in ACTIONS.values():
-        print "{}: {}".format(action.cmdstring, action.description)
-
 class Action(object):
     def __init__(self, cmdstring, cmdfunc, description):
         self.cmdstring = cmdstring
@@ -14,8 +7,40 @@ class Action(object):
         self.description = description
 
 ACTIONS = {}
+
 def register_action(cmdstring, cmdfunc, description):
     ACTIONS[cmdstring] = Action(cmdstring, cmdfunc, description)
 
+def quit(*args, **kwargs):
+    sys.exit(0)
+
+def help(*args, **kwargs):
+    for action in ACTIONS.values():
+        print "{}: {}".format(action.cmdstring, action.description)
+
+def remove_organization_user(trello, settings, org):
+    members = org.get_members()
+    for i, user in enumerate(members):
+        print "{}) {} ({})".format(i, user.full_name, user.username)
+    cmd = ""
+    while cmd != 'q':
+        cmd = raw_input("Which one do you want to remove? (q to quit)")
+        try:
+            cmd = int(cmd)
+        except ValueError: continue
+        user_to_remove = members[cmd]
+        print "removing {}".format(user_to_remove.full_name)
+        import ipdb; ipdb.set_trace()
+
+def cleanup_old_members_from_boards(trello, settings, org):
+    # TODO
+    # what this should do is go through the org boards looking for folks who aren't members and offer to remove them from the board
+    pass
+
+def remove_members_from_a_board(trello, settings, org):
+    # TODO
+    pass
+
 register_action('q', quit, 'Quit')
 register_action('h', help, 'Help')
+register_action('remove_user', remove_organization_user, 'Remove a user from the organization')
--- run.py
+++ run.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
-# can list organization users
-# can remove an organization user
-# can check boards for users that shouldn't be on there
-# can list organization boards
-# can set task board
-# can set epic board
 
 from actions import ACTIONS
+
+from trello import TrelloClient
+import settings
+
+trelloclient = TrelloClient(
+    api_key=settings.TRELLO_API_KEY,
+    api_secret=settings.TRELLO_API_SECRET,
+    token=settings.TRELLO_OAUTH_TOKEN,
+)
+org = trelloclient.get_organization(settings.TRELLO_ORGANIZATION_ID)
 
 if __name__ == '__main__':
     while True:
@@ -15,4 +19,4 @@ if __name__ == '__main__':
         if action is None:
             print "Unknown action. 'h' for help"
         else:
-            action.cmdfunc()
+            action.cmdfunc(trello=trelloclient, settings=settings, org=org)
