d517a22b303f79d32ccd8f27b6c37290e0019969
Jeff Schwaber <jschwaber@mobilepaks.com>
2015-12-28 21:09:13 -0800

    almost there on using asyncio/trollius to fill in the database from the API

--- models.py
+++ models.py
@@ -1,31 +1,41 @@
 #!/usr/bin/env python
 
 import settings
-from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy import Column, String, Integer, ForeignKey, Boolean
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+class AddObjectBase(object):
+    @classmethod
+    def create(cls, session, **kwargs):
+        foo = cls(**kwargs)
+        session.add(foo)
+        foo = session.query(cls).filter_by(**kwargs).first()
+        return foo
 
-class Board(Base):
+
+class Board(Base, AddObjectBase):
     __tablename__ = 'board'
     id = Column(String, primary_key=True)
     name = Column(String)
+    epic_board = Column(Boolean, default=False)
+    story_board = Column(Boolean, default=False)
 
-class TrelloList(Base):
+class TrelloList(Base, AddObjectBase):
     __tablename__ = 'list'
     id = Column(String, primary_key=True)
     name = Column(String)
     board = relationship(Board)
     board_id = Column(String, ForeignKey(Board.id))
 
-class Checklist(Base):
+class Checklist(Base, AddObjectBase):
     __tablename__ = 'checklist'
     id = Column(String, primary_key=True)
     name = Column(String)
 
-class Card(Base):
+class Card(Base, AddObjectBase):
     __tablename__ = 'card'
     id = Column(String, primary_key=True)
     name = Column(String)
@@ -33,6 +43,8 @@ class Card(Base):
     trellolist_id = Column(String, ForeignKey(TrelloList.id))
     magic_checklist = relationship(Checklist)
     magic_checklist_id = Column(String, ForeignKey(Checklist.id))
+    connected_to_id = Column(String, ForeignKey("card.id"))
+    connected_to = relationship("Card", remote_side=[id])
 
 def get_session():
     from sqlalchemy import create_engine
--- trellointerface.py
+++ trellointerface.py
@@ -17,44 +17,73 @@ class TrelloInterface(object):
         self.db_session.query(Board).delete()
 
         self._story_board = self.trello.get_board(self.settings.CURRENT_TASK_BOARD)
-        self.db_session.add(Board(id=self._story_board.id, name=self._story_board.name))
-
-        self._story_lists = self._story_board.get_lists('open')
-
         self._epic_board = self.trello.get_board(self.settings.CURRENT_EPIC_BOARD)
+        self.db_session.add(Board(id=self._story_board.id, name=self._story_board.name, story_board=True))
+        self.db_session.add(Board(id=self._epic_board.id, name=self._epic_board.name, epic_board=True))
+        self._story_lists = self._story_board.get_lists('open')
         self._epic_lists = self._epic_board.get_lists('open')
-        self._story_list_cards = []
-        self._epic_list_cards = []
         self._get_epics_and_stories()
 
     def _get_epics_and_stories(self):
-        def _get(lst):
+
+        def _get_checklist_future(trellocard):
+            meta_checklist = None
+            db_card = Card.create(self.db_session, id=trellocard.id, name=trellocard.name, trellolist_id=trellocard.list_id)
+            # gotta populate those checklists
+            trellocard.fetch(eager=True)
+            list_name = None
+            if db_card.trellolist.board.story_board:
+                list_name = 'Meta'
+            elif db_card.trellolist.board.epic_board:
+                list_name = 'Stories'
+            else:
+                raise Exception("Board must be epic or stories")
+
+            if trellocard.checklists:
+                for checklist in trellocard.checklists:
+                    if checklist.name == list_name:
+                        meta_checklist = checklist
+            if not meta_checklist:
+                meta_checklist = trellocard.add_checklist(list_name, [])
+            db_card.magic_checklist_id = meta_checklist.id
+            if db_card.trellolist.board.story_board:
+                for item in meta_checklist.items:
+                    if item['name'].startswith('Epic Connection:'):
+                        db_card.connected_to_id = item['name'].split(':')[1].strip()
+            db_card.save()
+
+        def _make_checklist_futures(cards):
+            loop = trollius.get_event_loop()
+            futures = []
+            for card in cards:
+                futures.append(loop.run_in_executor(None, _get_checklist_future, card))
+            return futures
+
+        def _make_trello_list(lst):
             self.db_session.add(TrelloList(id=lst.id, name=lst.name, board_id=lst.board.id))
             return lst.list_cards()
 
-        def _do_calls():
+        def _make_cards():
             loop = trollius.get_event_loop()
-            story_futures = []
-            epic_futures = []
+            futures = []
             for story_list in self._story_lists:
-                story_futures.append(loop.run_in_executor(None, _get, story_list))
+                futures.append(loop.run_in_executor(None, _make_trello_list, story_list))
             for epic_list in self._epic_lists:
-                epic_futures.append(loop.run_in_executor(None, _get, epic_list))
-            return (story_futures, epic_futures)
+                futures.append(loop.run_in_executor(None, _make_trello_list, epic_list))
+            return futures
 
         @trollius.coroutine
-        def call():
-            all_futures = _do_calls()
-            for futures in all_futures:
-                for future in futures:
-                    cards = yield trollius.From(future)
-                    for card in cards:
-                        self.db_session.add(Card(id=card.id, name=card.name, trellolist_id=card.list_id))
-            raise trollius.Return("")
+        def make_cards():
+            futures = _make_cards()
+            for future in futures:
+                cards = yield trollius.From(future)
+                new_futures = _make_checklist_futures(cards)
+                for future in new_futures:
+                    yield trollius.From(future)
 
         now = datetime.datetime.now()
         loop = trollius.get_event_loop()
-        loop.run_until_complete(call())
+        loop.run_until_complete(make_cards())
         self.db_session.commit()
         print "time elapsed: {}".format(datetime.datetime.now() - now)
 
