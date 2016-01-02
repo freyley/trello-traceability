c46ea602072ac7c9f4572ef85faa9df582a6f450
Jeff Schwaber <jschwaber@mobilepaks.com>
2016-01-02 13:23:33 -0800

    remove old code that's not used anymore

--- base_action.py
+++
@@ -1,5 +0,0 @@
-class Action(object):
-    def __init__(self, cmdstring, cmdfunc, description):
-        self.cmdstring = cmdstring
-        self.cmdfunc = cmdfunc
-        self.description = description
