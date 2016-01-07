d3099f7ae682923cbfbed28cdcd45e40c6bc561e
Jeff Schwaber <jschwaber@mobilepaks.com>
2016-01-06 22:46:18 -0800

    engineering time calculation, too

--- cmds.py
+++ cmds.py
@@ -35,14 +35,21 @@ def find_boards():
     for board in organization.get_boards('open'):
         print "{} ({})".format(board.name, board.id)
 
+def _check_item(action_list, key, value, startswith):
+    return action_list[key] == value or (startswith and action_list[key].startswith(value))
+
 def _identify_list_from_identifiers(action_list, identify_mechanisms):
     for key, value in identify_mechanisms.items():
+        startswith = False
+        if '__startswith' in key:
+            key = key.split('__')[0]
+            startswith = True
         if isinstance(value, list) or isinstance(value, tuple):
             for item in value:
-                if action_list[key] == item:
+                if _check_item(action_list, key, item, startswith):
                     return True
         else:
-            if action_list[key] == value:
+            if _check_item(action_list, key, value, startswith):
                 return True
     return False
 
@@ -55,6 +62,15 @@ def _going_to_a_started_list(action):
     import settings
     action_list = action['data']['listAfter']
     return _identify_list_from_identifiers(action_list, settings.IDENTIFY_STARTING_LIST)
+
+def _leaving_doing_for_committed(action):
+    import settings
+    before_list = action['data']['listBefore']
+    is_currently_doing = _identify_list_from_identifiers(before_list, settings.IDENTIFY_CURRENTLY_DOING_LIST)
+    after_list = action['data']['listAfter']
+    going_to_done = _identify_list_from_identifiers(after_list, settings.IDENTIFY_ANY_DONE_LIST)
+    return bool(is_currently_doing and going_to_done)
+
 
 def _parse_date_or_ipdb(datey):
     try:
@@ -116,49 +132,66 @@ def find_cycle_times():
     CYCLE_TIME_IDENTIFIER = 'Cycle time:::'
     FINISHED_IDENTIFIER = 'Finished:::'
     STARTED_IDENTIFIER = 'Started:::'
+    ENGINEERING_IDENTIFIER = "Committed:::"
     cycle_time_file = open(settings.CYCLE_TIMES_CSV_LOCATION, 'a')
-    for board_id in [settings.CURRENT_STORY_BOARD, settings.FUTURE_STORY_BOARD]:
-        board = trelloclient.get_board(board_id)
+    board = trelloclient.get_board(settings.CURRENT_STORY_BOARD)
 
-        board.fetch_actions("updateCard:idList")
-        actions = board.actions
-        for action in actions:
-            action_date = dateutil.parser.parse(action['date'])
-            if _going_to_a_started_list(action):
-                card = trelloclient.get_card(action['data']['card']['id'])
-                card.fetch(eager=True)
-                for checklist in card.checklists:
-                    if checklist.name == 'Meta':
-                        checklist.found_started = False
-                        for item in checklist.items:
-                            if item['name'].startswith(STARTED_IDENTIFIER):
-                                checklist.found_started = True
-                        if not checklist.found_started:
-                            checklist.add_checklist_item('{} {}'.format(STARTED_IDENTIFIER, action['date']))
-            elif _going_to_a_done_list(action):
-                card = trelloclient.get_card(action['data']['card']['id'])
-                card.fetch(eager=True)
-                for checklist in card.checklists:
-                    if checklist.name == 'Meta':
-                        checklist.found_done = False
-                        checklist.started_time = checklist.finished_time = checklist.cycle_time = None
-                        for item in checklist.items:
-                            if item['name'].startswith(FINISHED_IDENTIFIER):
-                                checklist.found_done = True
-                                checklist.finished_time = _parse_date_or_ipdb(item['name'].split(FINISHED_IDENTIFIER)[1])
-                            if item['name'].startswith(STARTED_IDENTIFIER):
-                                checklist.started_time = _parse_date_or_ipdb(item['name'].split(STARTED_IDENTIFIER)[1])
-                            if item['name'].startswith(CYCLE_TIME_IDENTIFIER):
-                                checklist.cycle_time = int(item['name'].split(CYCLE_TIME_IDENTIFIER)[1])
-                        if not checklist.found_done:
-                            checklist.add_checklist_item('{} {}'.format(FINISHED_IDENTIFIER, action['date']))
-                        if checklist.started_time and checklist.finished_time:
-                            cycle_time = (action_date - checklist.started_time).total_seconds() / (60*60)
-                            cycle_time_string = '{} {}'.format(CYCLE_TIME_IDENTIFIER, cycle_time)
-                            if checklist.cycle_time is None:
-                                checklist.add_checklist_item(cycle_time_string)
-                                cycle_time_file.writelines(["{card.name},{card.id},{card.url},{cycle_time}".format(card=card, cycle_time=cycle_time)])
-                            print "Found cycle time for {}: {}".format(card.name, cycle_time_string)
+    board.fetch_actions("updateCard:idList")
+    actions = board.actions
+    for action in actions:
+        action_date = dateutil.parser.parse(action['date'])
+        if _going_to_a_started_list(action):
+            card = trelloclient.get_card(action['data']['card']['id'])
+            card.fetch(eager=True)
+            for checklist in card.checklists:
+                if checklist.name == 'Meta':
+                    checklist.found_started = False
+                    for item in checklist.items:
+                        if item['name'].startswith(STARTED_IDENTIFIER):
+                            checklist.found_started = True
+                    if not checklist.found_started:
+                        checklist.add_checklist_item('{} {}'.format(STARTED_IDENTIFIER, action['date']))
+        elif _leaving_doing_for_committed(action):
+            card = trelloclient.get_card(action['data']['card']['id'])
+            card.fetch(eager=True)
+            for checklist in card.checklists:
+                if checklist.name == 'Meta':
+                    checklist.found_engineering = False
+                    for item in checklist.items:
+                        if item['name'].startswith(ENGINEERING_IDENTIFIER):
+                            checklist.found_engineering = True
+                    if not checklist.found_engineering:
+                        checklist.add_checklist_item('{} {}'.format(ENGINEERING_IDENTIFIER, action['date']))
+        elif _going_to_a_done_list(action):
+            card = trelloclient.get_card(action['data']['card']['id'])
+            card.fetch(eager=True)
+            for checklist in card.checklists:
+                if checklist.name == 'Meta':
+                    checklist.found_done = False
+                    checklist.started_time = checklist.finished_time = checklist.cycle_time = None
+                    for item in checklist.items:
+                        if item['name'].startswith(FINISHED_IDENTIFIER):
+                            checklist.found_done = True
+                            checklist.finished_time = _parse_date_or_ipdb(item['name'].split(FINISHED_IDENTIFIER)[1])
+                        if item['name'].startswith(STARTED_IDENTIFIER):
+                            checklist.started_time = _parse_date_or_ipdb(item['name'].split(STARTED_IDENTIFIER)[1])
+                        if item['name'].startswith(ENGINEERING_IDENTIFIER):
+                            checklist.engineering_time = _parse_date_or_ipdb((item['name'].split(ENGINEERING_IDENTIFIER)[1]))
+                        if item['name'].startswith(CYCLE_TIME_IDENTIFIER):
+                            checklist.cycle_time = float(item['name'].split(CYCLE_TIME_IDENTIFIER)[1])
+                    if not checklist.found_done:
+                        checklist.add_checklist_item('{} {}'.format(FINISHED_IDENTIFIER, action['date']))
+                    if checklist.started_time and checklist.finished_time:
+                        engineering_time = ""
+                        if checklist.engineering_time:
+                            engineering_time = (action_date - checklist.engineering_time).total_seconds() / (60 * 60)
+                        cycle_time = (action_date - checklist.started_time).total_seconds() / (60*60)
+                        cycle_time_string = '{} {}'.format(CYCLE_TIME_IDENTIFIER, cycle_time)
+                        if checklist.cycle_time is None:
+                            checklist.add_checklist_item(cycle_time_string)
+                            cycle_time_file.write('"{card.name}",{card.id},"{card.url}",{engineering_time},{cycle_time}\n'.format(
+                                card=card, cycle_time=cycle_time, engineering_time=engineering_time))
+                        print "Found cycle time for {}: {}".format(card.name, cycle_time_string)
 
 
 
